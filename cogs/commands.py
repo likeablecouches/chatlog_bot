82c9fa72f13ed9edc84bef2ee92e72d945c54db3
kevincao <ilikeeatingforlife@gmail.com>
2021-07-18 18:19:20 -0700

    split code for writing to files into functions

--- cogs/commands.py
+++ cogs/commands.py
@@ -5,6 +5,35 @@ from bot import bot
 
 import os
 
+logs_dir = 'logs'
+
+# functions
+def write_header(channel, file):
+    file.write(f'Channel: {channel.name}\n'\
+        + f'Guild: {channel.guild.name}\n'\
+        + f'Category: {channel.category.name}\n\n'\
+        #+ f'Created at: {channel.created_at[0:3]}\n\n')
+        + '========================================\n\n')
+
+def write_messages(mesg_list, file):
+    for mesg in reversed(mesg_list):
+        file.write(f'[{mesg.created_at}]\n'\
+            + f'{mesg.author.display_name}({mesg.author.name}'\
+            + f'#{mesg.author.discriminator}): {mesg.content}\n\n')
+
+        if mesg.attachments:
+            for attachment in mesg.attachments:
+                file.write(f'{{Attachment(s)}}:\n'\
+                    + f'Name:       {attachment.filename}\n'\
+                    + f'Type:       {attachment.content_type}\n'\
+                    + f'Size:       {attachment.size} byte(s)\n'\
+                    + f'Url:        {attachment.url}\n\n')
+
+def write_footer(mesg_num, file):
+    file.write('========================================\n\n'\
+        + f'Logged {mesg_num} messages(s)')
+
+# class
 class Commands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -26,10 +55,13 @@ class Commands(commands.Cog):
             await ctx.channel.purge(limit=int(amount))
 
     @commands.command()
-    async def logchannel(self, ctx, channel_id, percentage=100, \
+    async def logchannel(self, ctx, channel_id=None, percentage=100, \
         filename=None):
 
-        channel = bot.get_channel(int(channel_id))
+        if channel_id == None:
+            channel = ctx.channel
+        else:
+            channel = bot.get_channel(int(channel_id))
 
         if filename == None:
             filename = channel.name
@@ -39,14 +71,23 @@ class Commands(commands.Cog):
         percentage = float(percentage) / 100
         
         messages = await channel.history(limit=None).flatten()
-        
-        logs_dir = 'logs'
 
         if percentage < 1:
             messages = messages[:int(len(messages) * percentage)]
 
         if logs_dir not in os.listdir():
-            os.mkdir(logs_dir, 755)
+            oldmask = os.umask(000)
+            os.mkdir(logs_dir, 0o755)
+            os.umask(oldmask)
+        
+        with open(f'./{logs_dir}/{filename}', 'w') as logfile:
+            # write channel info
+            write_header(channel, logfile)
+            write_messages(messages, logfile)
+            write_footer(len(messages), logfile)
+
+        with open(f'./{logs_dir}/{filename}', 'r') as logfile:
+            await ctx.send(file=discord.File(fp=logfile, filename=filename))
 
         '''
             1. set channel var to channel object by channel id
